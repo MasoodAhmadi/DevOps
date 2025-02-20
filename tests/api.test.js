33b590d9e102bdad9f98a2aa438ea04554955af6
Masood <masood.ahmadi@tuni.fi>
2025-02-20 10:55:46 +0200

    Update CI pipeline for Service2 API tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -3,12 +3,12 @@ const API_URL = process.env.API_URL || "http://localhost:8198/fetch";
 
 describe('Service2 API Tests', () => {
     test('GET /api/system-info should return 200 and valid JSON', async () => {
-        // // const response = await request(url).get('/api/system-info').auth('user1', '123456');
-        // const response = await request(API_URL).get("/").auth('user1', '123456')
-        // expect(response.status).toBe(200);
-        // expect(response.body).toHaveProperty('service');
-        // expect(response.body).toHaveProperty('ip');
-        console.log("i am masood")
+        console.log("Test is executed")
+        // const response = await request(API_URL).get('/api/system-info').auth('user1', '123456');
+        const response = await request(API_URL).get("/").auth('user1', '123456')
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('service');
+        expect(response.body).toHaveProperty('ip');
     });
 
 });
