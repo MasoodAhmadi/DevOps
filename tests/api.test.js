081aa6bab543c769a6b6b1974e39fb6a3b2964ea
Masood <masood.ahmadi@tuni.fi>
2025-02-20 12:25:58 +0200

    Update CI pipeline for Service2 API tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,14 +1,20 @@
-const request = require('supertest');
+// const request = require('supertest');
+const axios = require('axios');
 const API_URL = process.env.API_URL || "http://nginx:8198/fetch";
 
 describe('Service2 API Tests', () => {
     test('GET /api/system-info should return 200 and valid JSON', async () => {
         // // const response = await request(url).get('/api/system-info').auth('user1', '123456');
-        const response = await request(API_URL).get("/").auth('user1', '123456')
+        console.log("i am here")
+        const response = await axios.get(API_URL, {
+            auth: {
+                username: 'user1',
+                password: '123456'
+            }
+        })
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('service');
         expect(response.body).toHaveProperty('ip');
-        console.log("i am masood")
     });
 
 });
